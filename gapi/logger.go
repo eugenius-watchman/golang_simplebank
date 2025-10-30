78fe05510ae8beef98f7bba94e5c3b661177d913
eugenius-watchman <eugenius4real@gmail.com>
2025-10-30 09:00:33 +0000

    refactor: Enhance logging interceptor configuration
    - Optimize gRPC logger setup and interceptor registration
    - Improve logging middleware initialization and error handling
    - Refine main application startup sequence for better logging integration
    - Streamline logger configuration and interceptor chain setup
    
    This improves logging reliability and application startup consistency

--- gapi/logger.go
+++ gapi/logger.go
@@ -2,6 +2,7 @@ package gapi
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -17,26 +18,70 @@ func GrpcLogger(
 	req any, 
 	info *grpc.UnaryServerInfo, 
 	handler grpc.UnaryHandler,
-	) (resp any, err error) {
+) (resp any, err error) {
+	startTime := time.Now()
+	result, err := handler(ctx, req)
+	duration := time.Since(startTime)
+
+	statusCode := codes.Unknown
+	if st, ok := status.FromError(err); ok {
+		statusCode = st.Code()
+	}
+
+	logger := log.Info()
+	if err != nil {
+		logger = log.Error().Err(err)
+	}
+
+	logger.Str("protocol", "grpc").
+		Str("method", info.FullMethod).
+		Int("status_code", int(statusCode)).
+		Str("status_text", statusCode.String()).
+		Dur("duration", duration).
+		Msg("received a gRPC request")
+
+	return result, err
+}
+
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode 	int
+	Body		[]byte
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rec *ResponseRecorder) Write(body []byte) (int, error) {
+	rec.Body = body
+	return rec.ResponseWriter.Write(body)
+}
+
+// http logger middleware func
+func HTTPLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		startTime := time.Now()
-		result, err := handler(ctx, req)
+		rec := &ResponseRecorder{
+			ResponseWriter: res,
+			StatusCode: 	http.StatusOK,
+		}
+
+		handler.ServeHTTP(rec, req)
 		duration := time.Since(startTime)
 
-		statusCode := codes.Unknown
-		if st, ok := status.FromError(err); ok {
-			statusCode = st.Code()
-		}
-
 		logger := log.Info()
-		if err != nil {
-			logger = log.Error().Err(err)
+		if rec.StatusCode != http.StatusOK {
+			logger = log.Error().Bytes("body", rec.Body)
 		}
 
-		logger.Str("protocol", "grpc").
-			Str("method", info.FullMethod).
-			Int("status_code", int(statusCode)).
-			Str("status_text", statusCode.String()).
+		logger.Str("protocol", "http").
+			Str("method", req.Method).
+			Str("path", req.RequestURI).
+			Int("status_code", rec.StatusCode).
+			Str("status_text", http.StatusText(rec.StatusCode)).
 			Dur("duration", duration).
-			Msg("received a gRPC request")
-		return result, err
-	}
+			Msg("received a HTTP request")
+	})
+}
